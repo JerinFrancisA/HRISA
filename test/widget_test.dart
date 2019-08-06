7c21d24fe67ed6f0e8efe64cc4d218680bb8f04e
Anirudh <anirudhr40@gmail.com>
2019-08-06 12:37:12 +0530

    Added correct OTP files xD

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:viren_demo/main.dart';
+import 'package:firephoneauth/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
